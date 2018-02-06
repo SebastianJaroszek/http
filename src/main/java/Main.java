de8f98191cb5db8634390bca6551e9e3384fe3fd
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-02-06 20:41:57 +0100

    wersja działająca

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,32 +4,57 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
         OkHttpClient client = new OkHttpClient();
         Request request;
         Response response;
 
+        System.out.println("Wprowadź identyfikator osoby:");
+        int personId = sc.nextInt();
+
         request = new Request.Builder()
-                .url("http://httpbin.org/ip")
-                //.url("https://swapi.co/api/people/1/")
+                //.url("http://httpbin.org/ip")
+                .url("https://swapi.co/api/people/" + personId)
                 .build();
 
         response = client.newCall(request).execute();
 
         ObjectMapper objectMapper = new ObjectMapper();
 
+        People person = null;
+
         if (response.code() >= 200 && response.code() < 300) {
             //System.out.println(response.body().string());
-            System.out.println(response.code());
-            System.out.println(response.message());
-            Ip ip = objectMapper.readValue(response.body().string(), Ip.class);
-            System.out.println(ip.getOrigin());
+            //System.out.println(response.code());
+            //System.out.println(response.message());
+            //Ip ip = objectMapper.readValue(response.body().string(), Ip.class);
+            //System.out.println(ip.getOrigin());
+            person = objectMapper.readValue(response.body().string(), People.class);
+            System.out.println("size: " + person.getFilms().size());
         } else {
             System.out.println(response.message());
         }
+
+        if (person != null) {
+            System.out.println("Wprowadź identyfikator filmu od 1 do " + person.getFilms().size() + ":");
+            int filmIndex = sc.nextInt();
+            Request filmRequest = new Request.Builder()
+                    .url(person.getFilms().get(filmIndex-1))
+                    .build();
+
+            Response filmResponse = client.newCall(filmRequest).execute();
+
+            Film film = objectMapper.readValue(filmResponse.body().string(), Film.class);
+
+            System.out.println(film.getTitle());
+        }
+
     }
 
 }
